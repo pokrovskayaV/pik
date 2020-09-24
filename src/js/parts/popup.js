7a186480f317d0230a8926c417664b30c96c7e9b
My Name <myEmail@example.com>
2020-09-24 16:05:47 +0300

    final2

--- src/js/parts/popup.js
+++ src/js/parts/popup.js
@@ -1,6 +1,12 @@
 export default function initPopup() {
   $(".menu__popap").magnificPopup({
     type: "inline",
-    closeOnBgClick: true
+    removalDelay: 400, //delay removal by X to allow out-animation
+    callbacks: {
+      beforeOpen: function() {
+         this.st.mainClass = this.st.el.attr('data-effect');
+      }
+    },
+    midClick: true 
   });
 }
